9b4235732765d390c1a2e35b88d6eefefb8e723c
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-12-22 01:24:40 -0300

    Class component added

---
+++ Pack05/pack05app/src/components/CourseTopicsClass.js
@@ -0,0 +1,30 @@
+// Class components theme
+import React from 'react'
+
+class CourseTopicsClass extends React.Component {
+    render() {
+        return (
+            <>
+                <h2> FrontEnd course topics: </h2>
+                <ul>
+                    <li>
+                        HTML structure
+                    </li>
+                    <li>
+                        CSS styles
+                    </li>
+                    <li>
+                        JavaScript fundamentals
+                    </li>
+                    <li>
+                        ReactJS essentials
+                    </li>
+                    <li>
+                        API use
+                    </li>
+                </ul>
+                )
+            </>
+        )
+    }
+}
