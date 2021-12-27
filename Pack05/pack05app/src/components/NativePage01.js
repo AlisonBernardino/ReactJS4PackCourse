7f81b63871308095a346fba39b8218e7a88568b5
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-12-27 15:52:45 +0000

    New project added and old files removed

--- Pack05/pack05app/src/components/NativePage01.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NativePage01() {
-    return (
-        <div>
-            <h2> FrontEnd course - Details </h2>
-        </div>
-    )
-}
--- Pack05/pack05app/src/components/NativePage02.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NativePage02(){
-    return(
-        <div>
-            <h2> BackEnd course - Details </h2>
-        </div>
-    )
-}
