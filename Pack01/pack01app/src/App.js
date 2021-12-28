02d4483b6f15dd382904df5a8a9615b71e5cc905
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-12-28 06:18:27 +0000

    App.js changes added

--- Pack01/pack01app/src/App.js
+++ Pack01/pack01app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MainComponentButton extends Component {
+  render() {
+    return (
+      <button
+        onClick={() => { this.props.handleClick(this.props.label); }}
+      >
+        {this.props.label}
+      </button>
+    );
+  }
+}
+
+class MainComponentLabel extends Component {
+  render() {
+    return <p>Clicked button: {this.props.text}</p>
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonText: '',
+    };
+  }
+
+  setButtonText = (buttonText) => {
+    this.setState({ buttonText });
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <MainComponentLabel text={this.state.buttonText} />
+        {/* <MainComponentLabel labelText="Car management panel" alternativeLabel="Press the buttons to change the vehicle data" isTurnedOn={false}/> */}
+        <MainComponentButton handleClick={this.setButtonText} label="Change color" />
+        <MainComponentButton handleClick={this.setButtonText} label="Button model" />
+        <MainComponentButton handleClick={this.setButtonText} label="Performance kit ON/OFF" />
+        <MainComponentButton handleClick={this.setButtonText} label="Fueltank ON/OFF" />
+      </div>
+    );
+  }
 }
 
 export default App;
