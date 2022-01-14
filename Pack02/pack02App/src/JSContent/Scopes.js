6b5a95ee87b0e096beceb226b57f9ae4b5c7e423
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2022-01-14 13:27:28 +0000

    Scope shadowing added

--- Pack02/pack02App/src/JSContent/Scopes.js
+++ Pack02/pack02App/src/JSContent/Scopes.js
@@ -7,3 +7,20 @@ anObject.name = 'JSCoder';
 anObject = { name: 'JavaCoder'};
 
 
+// Variable shadowing
+
+let userAge1 = 15;
+let userAge2 = 17;
+
+function checkOfficialAge(){
+    userAge1 = 16;
+    let userAge2 = 18;
+}
+
+checkOfficialAge();
+console.log(userAge1);
+console.log(userAge2);
+
+
+
+
